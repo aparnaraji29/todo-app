3e5e9700c635ee86b7e0c18280f0911c29cd9b41
aparna <aparnaraji2000@gmail.com>
2023-02-18 23:24:54 +0530

    final change

--- js/login.js
+++ js/login.js
@@ -1,83 +1,4 @@
-// let username = document.getElementById("username");
-// let password = document.getElementById("password");
-// let pswrderror = document.getElementById("pswrd-error");
-// let usererror = document.getElementById("user-error");
-// let successIcon = document.getElementsByClassName("success-icon");
-// let failureIcon = document.getElementsByClassName("failure-icon");
-
-// function validateName(){
-//     if(username.value.trim()==""){
-//         usererror.innerHTML="username cannot be empty"
-//         usererror.style.color="red"
-//         username.style.border = "2px solid red";
-//         failureIcon.username.style.opacity = "1";
-//         successIcon.style.opacity = "0";
-//         return false
-//     }
-
-
-//     else if(username.value!=="admin"){
-//         usererror.innerHTML=" invalid username"
-//         usererror.style.color="red"
-//         username.style.border = "2px solid red";
-//         failureIcon.style.opacity = "1";
-//         successIcon.style.opacity = "0";
-//         return false
-//     }
-
-//     else if(username.value==="admin"){
-//         usererror.innerHTML=" valid username"
-//         usererror.style.color="green"
-//         username.style.border = "2px solid green";
-//         failureIcon.style.opacity = "0";
-//         successIcon.style.opacity = "1";
-//         return true
-//     }
-
-
-// }
-
-
-
-// function validatePassword(){
-//     if(password.value.trim()==""){
-//         pswrderror.innerHTML="password cannot be empty"
-//         password.style.border = "2px solid red";
-//         pswrderror.style.color="red"
-//         return false
-//     }
-
-//     else if(password.value!=="12345"){
-//         pswrderror.innerHTML="invalid password"
-//         password.style.border = "2px solid red";
-//         pswrderror.style.color="red"
-//         return false
-//     }
-//     else if(password.value==="12345"){
-//         pswrderror.innerHTML="valid password"
-//         password.style.border = "2px solid green";
-//         pswrderror.style.color="green"
-//         return true
-//     }
-
-// }
-
-
-// function validateForm(){
-//     if(!validateName() || !validatePassword()){
-//         return false
-//     }
-//     else {
-//         return true
-//     }
-// }
-
-
-// function submitForm(event) {
-//     event.preventDefault(); // Prevent the form from submitting normally
-
-    // Get the input values
-    let username = document.getElementById("username");
+ let username = document.getElementById("username");
 let password = document.getElementById("password");
 let pswrderror = document.getElementById("pswrd-error");
 let usererror = document.getElementById("user-error");
@@ -140,19 +61,8 @@ function validatePassword(){
     }
 
 }
-// function submitForm(event) {
 
-//     event.preventDefault();
 
-//     // Check if the inputs are valid
-//     if (validateName() && validatePassword()) {
-//         // If the inputs are valid, redirect to another page
-//         return true // Replace this with the URL of the page you want to redirect to
-//     } else {
-//         // If the inputs are invalid, display an error message
-//      return false
-//     }
-// }
 
 		function validateForm() {
 			// Get the values of the username and password fields
@@ -165,7 +75,7 @@ function validatePassword(){
 
 			// Check if the username and password match the regular expressions
 			if (!usernameRegex.test(username)) {
-				alert("Invalid username");
+				alert("Username and Password cannot be blank");
 				return false;
 			}
 			if (!passwordRegex.test(password)) {
@@ -174,11 +84,10 @@ function validatePassword(){
                 
 			}
             if (usernameRegex.test(username)&&passwordRegex.test(password)) {
-				// alert("Invalid username");
+				//  alert("Username and Password cannot be blank");
 				return true;
             }
-			// Redirect to another page
-			//  window.location.href = "home.html";
+			
 		}
 
 
